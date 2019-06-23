7e57904d426632283447fd6f537da38beda87159
ankit1114 <46845315+ankit1114@users.noreply.github.com>
2019-06-23 22:23:32 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration Page</title>
+</head>
+<body align="centre">
+	<h2>Registration Page</h2>
+	       Username<br>
+	       <input type="text" name="username" class="username"><br><br>
+           Firstname <br>
+           <input type="text" name="username" class="username"><br><br>
+           Lastname <br>
+           <input type="text" name="username" class="username"><br><br>
+           email_id<br> 
+           <input type="text" name="username" class="username"><br><br>
+           Password<br>
+           <input type="text" name="username" class="username"><br><br>
+           Confirm Password<br> 
+           <input type="text" name="username" class="username"><br><br>
+           dob<br>
+           <input type="text" name="username" class="username"><br><br>
+           Contactnumber<br> 
+           <input type="text" name="username" class="username"><br><br>
+           <input type="submit" name="submit" value="submit"><br>
+</body>
+</html>
