bbf682f92395b3af02e56a594812888cf65ec488
ankit1114 <46845315+ankit1114@users.noreply.github.com>
2019-07-04 20:42:08 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+<body>
+<h2>Login</h2>
+<div>
+	<form action="login.php" method="post">
+      <div>
+      	<label for="user name"> <b> user name</b></label><br>
+		<input type="text" name="user name" required><br><br>	
+		<label for="password"> <b> password</b></label><br>
+		<input type="text" name="password" required><br><br>
+		<input type="submit" name="submit" value="signin">
+      </div>
+    
+	 </form>
+
+</div>
+
+</body>
+</html>
