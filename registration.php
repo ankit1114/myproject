1e7bb816a6174684069d22b04304d36dbe24972c
ankit1114 <46845315+ankit1114@users.noreply.github.com>
2019-07-05 07:29:44 +0530

    Add files via upload

--- registration.php
+++ registration.php
@@ -3,7 +3,10 @@
 <head>
 	<title>Registration Page</title>
 </head>
-<body align="centre">
+<body>
+	<div>
+		<form action="registration.php" method="post">
+			<div class="container">
 	<h2>Registration Page</h2>
 	       Username<br>
 	       <input type="text" name="username" class="username"><br><br>
@@ -21,6 +24,44 @@
            <input type="text" name="username" class="username"><br><br>
            Contactnumber<br> 
            <input type="text" name="username" class="username"><br><br>
-           <input type="submit" name="submit" value="submit"><br>
+           <br><input type="submit" name="submit" value="submit"><br>
+        </div>
+</form>
+</div>
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "taskone";
+
+	$conn=mysqli_connect($servername,$username,$password,$dbname);
+	
+	
+	if ($conn) {
+		echo" ";
+	}
+	else
+	{
+		die("Connection error: " . mysqli_connect_error());	
+	}
+
+	if(isset($_POST["submit"])){
+		$user_name = $_POST['user_name'];
+		$first_name = $_POST['first_name'];
+		$last_name = $_POST['last_name'];
+		$email_id = $_POST['email']; 
+		$password = $_POST['password'];
+		$confirm_password = $_POST['password'];
+		$d_o_b = $_POST['d_o_b'];
+		$contact_number = $_POST['contact_number'];
+		$sql = "INSERT INTO `taskistreg` (`id`,`user_name`, `first_name`, `last_name`, `email_id`, `password`, `confirm_password`, `contact_number`,`d_o_b`, ) VALUES (NULL, '$user_name', $first_name', '$last_name', '$email_id', '$password', '$confirm_password', '$contact_number', '$d_o_b' )";
+		if ($conn->query($sql) === TRUE) {
+		    echo "New record created successfully";
+		} else {
+		    echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+		$conn->close();
+	}
+?>
 </body>
 </html>
